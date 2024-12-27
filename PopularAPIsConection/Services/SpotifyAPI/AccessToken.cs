80fb0bc7646c5143da081b778efa943c6951236b
JoseLReyes <josreyes865@gmail.com>
2024-12-27 10:12:31 -0600

    Add project files.

---
+++ PopularAPIsConection/Services/SpotifyAPI/AccessToken.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PopularAPIsConection.Services.SpotifyAPI
+{
+
+    public class AccessToken
+    {
+        [JsonPropertyName("access_token")]
+        public string Token { get; set; }
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonPropertyName("expires_in")]
+        public int ExpiresIn { get; set; }
+    }
+
+}
